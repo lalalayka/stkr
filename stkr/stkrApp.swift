32be91f0e6f140862f4b62aee57a186fc36dcabd
sergey.kovalchuk <sergey.kovaclhuk@exness.com>
2026-01-02 14:12:04 +0200

    Initial Commit

---
+++ stkr/stkrApp.swift
@@ -0,0 +1,17 @@
+//
+//  stkrApp.swift
+//  stkr
+//
+//  Created by sergey.kovalchuk on 02/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct stkrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
